e3afe47077856f26359ea7c5d744908d600e273a
Jason Keene <jasonkeene@gmail.com>
2012-03-15 10:21:10 -0700

    Use coverage

--- runtests.py
+++ runtests.py
@@ -6,5 +6,9 @@ import nose
 
 
 if __name__ == '__main__':
-    nose_args = sys.argv + ['--with-doctest']
+    nose_args = sys.argv + [
+        '--with-doctest',
+        '--with-coverage',
+        '--cover-package=ubersmith',
+    ]
     nose.run(argv=nose_args)
