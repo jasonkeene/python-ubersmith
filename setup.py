6fc2e05f3ff710a88476350690fb172bedaac747
Jason Keene <jasonkeene@gmail.com>
2012-03-15 00:14:48 -0700

    Fixed requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from distutils.core import setup
 
 
+def get_requirements():
+    reqs = open('requirements.txt').read().strip().split('\n')
+    for i, r in enumerate(reqs):
+        for op in ['<', '>', '==', '<=', '>=', '!=']:
+            if op in r:
+                reqs[i] = u'{1} ({0}{2})'.format(op, *r.split(op, 1))
+    return reqs
+
 setup(
     name='ubersmith',
     version='0.0.1',
@@ -9,7 +17,7 @@ setup(
     description='Client library for the Ubersmith API 2.0',
     long_description=open('README.rst').read(),
     packages=['ubersmith'],
-    requires=open('requirements.txt').read().strip().split('\n'),
+    requires=get_requirements(),
     url='https://github.com/jasonkeene/python-ubersmith',
     license='MIT License',
     keywords=['ubersmith'],
