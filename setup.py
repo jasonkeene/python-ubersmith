b0dba6e0f0d0ff6262073734320fa9db23d7f806
Jason Keene <jasonkeene@gmail.com>
2012-03-14 22:53:01 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+
+setup(
+    name='ubersmith',
+    packages=['ubersmith'],
+    version='0.0.1',
+    description='Client library for the Ubersmith API 2.0',
+    long_description=open('README.rst').read(),
+    author='Jason Keene',
+    author_email='jasonkeene@gmail.com',
+    url='https://github.com/jasonkeene/python-ubersmith',
+    license='MIT License',
+    keywords=['ubersmith'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
