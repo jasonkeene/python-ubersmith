a35618ead059e4c6f67eb49d0e04e09bc9273de4
Jason Keene <jasonkeene@gmail.com>
2014-05-29 10:47:04 -0400

    Rev version number for PYPI.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ubersmith',
-    version='0.2.3',
+    version='0.2.4',
     author='Jason Keene',
     author_email='jasonkeene@gmail.com',
     description='Client library for the Ubersmith API 2.0',
