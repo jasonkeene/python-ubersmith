206c687570233c4d71063f1c688c25bbbd9847ab
Jason Keene <jasonkeene@gmail.com>
2014-06-02 21:29:02 -0400

    Refactor system tests to mock out requests library vs mocking out request handler.

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,26 +1,48 @@
 import datetime
 from decimal import Decimal
+import json
 
 from mock import Mock
 
+import ubersmith
+import ubersmith.api
 import ubersmith.order
 
-# TODO: setup/teardown module with default request handler
-# TODO: mock out requests library vs mocking out request handler
+
+def setup_module():
+    ubersmith.init(**{
+        'base_url': '',
+        'username': '',
+        'password': '',
+    })
 
 
-def test_order_list():
-    handler = Mock()
-    response = {
-        "60": {
-            "client_id": "50",
-            "activity": "1272400333",
-            "ts": "1272400333",
-            "total": "33.22",
-            "order_id": "60",
-        },
+def teardown_module():
+    ubersmith.api._DEFAULT_REQUEST_HANDLER = None
+
+
+def test_order_list(monkeypatch):
+    requests = Mock()
+    monkeypatch.setattr(ubersmith.api, 'requests', requests)
+    response = Mock()
+    response.headers = {
+        'content-type': 'application/json',
     }
-    handler.process_request.return_value = response
+    response.text = json.dumps({
+        "status": True,
+        "error_code": None,
+        "error_message": "",
+        "data": {
+            "60": {
+                "client_id": "50",
+                "activity": "1272400333",
+                "ts": "1272400333",
+                "total": "33.22",
+                "order_id": "60",
+            },
+        },
+    })
+    requests.post.return_value = response
     expected = {
         60: {
             u'client_id': 50,
@@ -30,5 +52,6 @@ def test_order_list():
             u'order_id': 60,
         }
     }
-    result = ubersmith.order.list(client_id=50, request_handler=handler)
+    result = ubersmith.order.list(client_id=50)
     assert expected == result
+
